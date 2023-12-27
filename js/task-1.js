188d50cc6918b67be04749bcc0ff9a91fbe6266d
StanVSV <stanislav2913vaivalov@gmail.com>
2023-12-27 22:17:10 +0100

    Homework-04.js-2

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@ function isEnoughCapacity(products, containerSize) {
     let totalProducts = 0;
 
     for (const quantity of Object.values(products)) {
-        totalProducts <= quantity;
+        totalProducts += quantity;
     }
     return totalProducts <= containerSize;
 }
